6061b17a8fcec6c8bfb8c3abad6b1296c5e1e0fe
hiroka <hrk.ismr.419forprgrm@gmail.com>
2021-07-12 18:49:54 +0900

    fixed

--- backend/resources/views/memo.blade.php
+++ backend/resources/views/memo.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <div class="h-100 bg-white">
     <div class="row h-100 m-0 p-0">
-
         <div class="col-3 h-100 m-0 p-0 border-left border-right border-gray">
-
             <div class="left-memo-menu d-flex justify-content-between pt-2">
                 <div class="pl-3 pt-2">
                     {{ $name }}さん、こんにちは。
@@ -17,7 +15,7 @@
             </div>
 
             <div class="left-memo-title h3 pl-3 pt-3">
-                メモリスト
+                リスト
             </div>
 
             <div class="left-memo-list list-group-flush p-0">
@@ -46,30 +44,27 @@
         </div>
 
         <div class="col-9 h-100">
-
-						@if ($select_memo)
-						<form class="w-100 h-100" method="post">
-								@csrf
-								<input type="hidden" name="edit_id" value="{{ $select_memo->id }}" />
-								<div id="memo-menu">
-										<button type="submit" class="btn btn-danger" formaction="{{ route('memo.delete') }}">
-												<i class="fas fa-trash-alt"></i>
-										</button>
-										<button type="submit" class="btn btn-success" formaction="{{ route('memo.update') }}">
-												<i class="fas fa-save"></i>
-										</button>
-								</div>
-								<input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="{{ $select_memo->title }}" />
-								<textarea id="memo-content" name="edit_content" placeholder="内容を入力する...">{{ $select_memo->content }}</textarea>
-						</form>
-						@else
-						<div class="mt-3 alert alert-info">
-								<i class="fas fa-info-circle"></i>メモを新規作成するか選択してください。
-						</div>
-						@endif
-
+            @if ($select_memo)
+            <form class="w-100 h-100" method="post">
+                @csrf
+                <input type="hidden" name="edit_id" value="{{ $select_memo->id }}" />
+                <div id="memo-menu">
+                    <button type="submit" class="btn btn-danger" formaction="{{ route('memo.delete') }}">
+                            <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button type="submit" class="btn btn-success" formaction="{{ route('memo.update') }}">
+                            <i class="fas fa-save"></i>
+                    </button>
+                </div>
+                <input type="text" id="memo-title" name="edit_title" placeholder="タイトルを入力する..." value="{{ $select_memo->title }}" />
+                <textarea id="memo-content" name="edit_content" placeholder="内容を入力する...">{{ $select_memo->content }}</textarea>
+            </form>
+            @else
+            <div class="mt-3 alert alert-info">
+                <i class="fas fa-info-circle"></i>メモを新規作成するか選択してください。
+            </div>
+            @endif
         </div>
-
     </div>
 </div>
 @endsection
